32eb46dd1701b77fa7963d4c0a2aa9dc442073ec
pancakes-proxy <zac_posey_12361_225@staffteam.learnhelp.cc>
2025-05-24 20:15:35 +0900

    Update application ID and namespace to match project structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.android.application")
+    id("com.internettools.tetoimagebrowser")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.tetoimagebrowser"
+    namespace = "com.internettools.tetoimagebrowser"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tetoimagebrowser"
+        applicationId = "com.internettools.tetoimagebrowser"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
